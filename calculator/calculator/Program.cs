748d2460865fb0092c4ab13a5a3a2a7129f8767d
Andrey Petkevich <pandreyn@gmail.com>
2016-03-04 10:16:57 +0300

    init commit

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        //calculator.exe 5 6 4 * + 1 -
+
+        static void Main(string[] args)
+        {
+            // The stack of integers not yet operated on
+            Stack<int> values = new Stack<int>();
+
+            string expression = "";
+            foreach (string arg in args)
+            {
+                expression += arg + " ";
+            }
+
+            Console.WriteLine("Expression: {0}", expression);
+
+            foreach (string token in args)
+            {
+                // if the value is an integer...
+                int value;
+                if (int.TryParse(token, out value))
+                {
+                    // ... push it to the stack
+                    values.Push(value);
+                }
+                else
+                {
+                    // otherwise evaluate the expression ...
+                    int rhs = values.Pop();
+                    int lhs = values.Pop();
+
+                    // ... and pop the result back tj the stack
+                    switch (token)
+                    {
+                        case "+":
+                            values.Push(lhs + rhs);
+                            break;
+                        case "-":
+                            values.Push(lhs - rhs);
+                            break;
+                        case "*":
+                            values.Push(lhs * rhs);
+                            break;
+                        case "/":
+                            values.Push(lhs / rhs);
+                            break;
+                        case "%":
+                            values.Push(lhs % rhs);
+                            break;
+                        default:
+                            throw new ArgumentException(string.Format("Unregognized token: {0}", token));
+
+                    }
+                }
+            }
+            Console.WriteLine("Result: {0}", values.Pop());
+            Console.WriteLine();
+            Console.WriteLine("Press [Return] to exit...");
+            Console.ReadLine();
+        }
+
+    }
+}
